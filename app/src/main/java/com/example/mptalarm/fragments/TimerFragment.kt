a31634916d2e92e3d1bdd8ee5fe9b55f536981c8
B1ZZARRO <isip_d.a.jeleznyak@mpt.ru>
2021-05-15 00:28:53 +0300

    14.05

--- app/src/main/java/com/example/mptalarm/fragments/TimerFragment.kt
+++ app/src/main/java/com/example/mptalarm/fragments/TimerFragment.kt
@@ -2,6 +2,7 @@ package com.example.mptalarm.fragments
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -18,10 +19,6 @@ import org.jetbrains.anko.onItemSelectedListener
 
 class TimerFragment : Fragment() {
 
-    val mainActivity = this.activity as MapsActivity
-    lateinit var pos1 : String
-    lateinit var pos2 : String
-
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
@@ -30,13 +27,10 @@ class TimerFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_timer, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
+    /*override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        loadData()
-        tp_dialog.setIs24HourView(true)
-        SpinnerChanged()
-        saveData()
-    }
+
+    }*/
 
     override fun onPause() {
         super.onDestroy()
@@ -44,45 +38,46 @@ class TimerFragment : Fragment() {
     }
 
     private fun saveData() {
-        pos1 = sp_dep1.selectedItemPosition.toString()
-        pos2 = sp_group1.selectedItemPosition.toString()
-        val insertedText = pos1
-        val insertedText1 = pos2
+        val mainActivity = this.activity as MapsActivity
+        val insertedText = sp_dep1.selectedItemPosition.toString()
+        val insertedText1 = sp_group1.selectedItemPosition.toString()
         val sharedPreferences = mainActivity.getSharedPreferences("sharedPrefs1", Context.MODE_PRIVATE)
         val editor = sharedPreferences.edit()
         editor.apply {
-            putString("string_KEY", insertedText.toString())
-            putString("string1_KEY", insertedText1.toString())
+            putString("string0_KEY", insertedText.toString())
+            putString("string10_KEY", insertedText1.toString())
         }.apply()
     }
 
     private  fun loadData() {
+        val mainActivity = this.activity as MapsActivity
         val sharedPreferences = mainActivity.getSharedPreferences("sharedPrefs1", Context.MODE_PRIVATE)
-        val savedString = sharedPreferences.getString("string_KEY", null)
-        val savedString1 = sharedPreferences.getString("string1_KEY", null)
-        pos1 = savedString!!
-        pos2 = savedString1!!
-
+        val savedString = sharedPreferences.getString("string0_KEY", null)
+        val savedString1 = sharedPreferences.getString("string10_KEY", null)
+        //sp_dep1.setSelection(savedString?.toInt()!!)
+        //sp_group1.setSelection(savedString1?.toInt()!!)
+        Log.i("lol", savedString!!)
+        Log.i("lol", savedString1!!)
     }
 
-   // override fun onViewCreated(view: View , savedInstanceState: Bundle?) {
-        //super.onViewCreated(view, savedInstanceState)
-
-    //}
+    override fun onViewCreated(view: View , savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        loadData()
+        tp_dialog.setIs24HourView(true)
+        SpinnerChanged()
+        btn_settings.setOnClickListener {
+            saveData()
+        }
+    }
     
     fun SpinnerChanged() {
-        if (pos1 != "" && pos2 != "") {
-            sp_dep1.setSelection(pos1.toInt())
-            sp_group1.setSelection(pos2.toInt())
-        }
-        else {
-            val adapter: ArrayAdapter<*> = ArrayAdapter.createFromResource(mainActivity, R.array.dep, android.R.layout.simple_spinner_item)
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            sp_dep1.setAdapter(adapter)
-            val adapter1: ArrayAdapter<*> = ArrayAdapter.createFromResource(mainActivity, R.array.group921, android.R.layout.simple_spinner_item)
-            adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            sp_group1.setAdapter(adapter1)
-        }
+        val mainActivity = this.activity as MapsActivity
+        val adapter: ArrayAdapter<*> = ArrayAdapter.createFromResource(mainActivity, R.array.dep, android.R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        sp_dep1.setAdapter(adapter)
+        val adapter1: ArrayAdapter<*> = ArrayAdapter.createFromResource(mainActivity, R.array.group921, android.R.layout.simple_spinner_item)
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        sp_group1.setAdapter(adapter1)
         sp_dep1.setOnItemSelectedListener(object : OnItemSelectedListener {
             override fun onItemSelected(adapterView: AdapterView<*>?, view: View, i: Int, l: Long) {
                 if (sp_dep1.selectedItemPosition == 0) {
