2a8271d4a7a0e7f6fb552904020fe34f40de7594
B1ZZARRO <isip_d.a.jeleznyak@mpt.ru>
2021-05-20 23:08:30 +0300

    20.05

--- app/src/main/java/com/example/mptalarm/fragments/TimerFragment.kt
+++ app/src/main/java/com/example/mptalarm/fragments/TimerFragment.kt
@@ -13,8 +13,10 @@ import com.example.mptalarm.MapsActivity
 import com.example.mptalarm.R
 import kotlinx.android.synthetic.main.fragment_timer.*
 
-
 class TimerFragment : Fragment() {
+
+    var dep1 : Int = -1
+    var group1 : Int = -1
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -36,87 +38,156 @@ class TimerFragment : Fragment() {
 
     private fun saveData() {
         val mainActivity = this.activity as MapsActivity
-        val insertedText = sp_dep1.selectedItemPosition.toString()
-        val insertedText1 = sp_group1.selectedItemPosition.toString()
+        val insertedText = dep1
+        val insertedText1 = group1
         val sharedPreferences = mainActivity.getSharedPreferences("sharedPrefs1", Context.MODE_PRIVATE)
         val editor = sharedPreferences.edit()
         editor.apply {
-            putString("string0_KEY", insertedText.toString())
-            putString("string10_KEY", insertedText1.toString())
+            putInt("string0_KEY", insertedText)
+            putInt("string10_KEY", insertedText1)
         }.apply()
     }
 
     private  fun loadData() {
         val mainActivity = this.activity as MapsActivity
         val sharedPreferences = mainActivity.getSharedPreferences("sharedPrefs1", Context.MODE_PRIVATE)
-        val savedString = sharedPreferences.getString("string0_KEY", null)
-        val savedString1 = sharedPreferences.getString("string10_KEY", null)
-        sp_dep1.setSelection(savedString?.toInt()!!)
-        sp_group1.setSelection(savedString1?.toInt()!!)
-        //Log.i("lol", savedString!!)
-        //Log.i("lol", savedString1!!)
+        val savedInt = sharedPreferences.getInt("string0_KEY",-1)
+        val savedInt1 = sharedPreferences.getInt("string10_KEY", -1)
+        dep1 = savedInt
+        group1 = savedInt1
     }
 
     override fun onViewCreated(view: View , savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         loadData()
+        tp_dialog.setIs24HourView(true)
         val mainActivity = this.activity as MapsActivity
         val adapter: ArrayAdapter<*> = ArrayAdapter.createFromResource(mainActivity, R.array.dep, android.R.layout.simple_spinner_item)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         sp_dep1.setAdapter(adapter)
-        val adapter1: ArrayAdapter<*> = ArrayAdapter.createFromResource(mainActivity, R.array.group921, android.R.layout.simple_spinner_item)
+        /*val adapter1: ArrayAdapter<*> = ArrayAdapter.createFromResource(mainActivity, R.array.group921, android.R.layout.simple_spinner_item)
         adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        sp_group1.setAdapter(adapter1)
+        sp_group1.setAdapter(adapter1)*/
+        SpinnerChanged()
+    }
+    
+    fun SpinnerChanged() {
+        val mainActivity = this.activity as MapsActivity
         sp_dep1.setOnItemSelectedListener(object : OnItemSelectedListener {
             override fun onItemSelected(adapterView: AdapterView<*>?, view: View, i: Int, l: Long) {
                 if (sp_dep1.selectedItemPosition == 0) {
                     val adapterr: ArrayAdapter<*> = ArrayAdapter.createFromResource(mainActivity, R.array.group921, android.R.layout.simple_spinner_item)
                     adapterr.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
                     sp_group1.setAdapter(adapterr)
+                    if (dep1 != -1 && group1 != -1) {
+                        sp_dep1.setSelection(dep1)
+                        sp_group1.setSelection(group1)
+                    }
+                    //SpinnerChanged()
+                    //SpinnerChanged1()
+                    btn_settings.setOnClickListener {
+                        dep1 = sp_dep1.selectedItemPosition
+                        group1 = sp_group1.selectedItemPosition
+                        saveData()
+                    }
                 }
                 else if (sp_dep1.selectedItemPosition == 1) {
                     val adapterr: ArrayAdapter<*> = ArrayAdapter.createFromResource(mainActivity, R.array.group926, android.R.layout.simple_spinner_item)
                     adapterr.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
                     sp_group1.setAdapter(adapterr)
+                    if (dep1 != -1 && group1 != -1) {
+                        sp_dep1.setSelection(dep1)
+                        sp_group1.setSelection(group1)
+                    }
+                    //SpinnerChanged()
+                    //SpinnerChanged1()
+                    btn_settings.setOnClickListener {
+                        dep1 = sp_dep1.selectedItemPosition
+                        group1 = sp_group1.selectedItemPosition
+                        saveData()
+                    }
                 }
                 else if (sp_dep1.selectedItemPosition == 2) {
                     val adapterr: ArrayAdapter<*> = ArrayAdapter.createFromResource(mainActivity, R.array.group927, android.R.layout.simple_spinner_item)
                     adapterr.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
                     sp_group1.setAdapter(adapterr)
+                    if (dep1 != -1 && group1 != -1) {
+                        sp_dep1.setSelection(dep1)
+                        sp_group1.setSelection(group1)
+                    }
+                    //SpinnerChanged()
+                    //SpinnerChanged1()
+                    btn_settings.setOnClickListener {
+                        dep1 = sp_dep1.selectedItemPosition
+                        group1 = sp_group1.selectedItemPosition
+                        saveData()
+                    }
                 }
                 else if (sp_dep1.selectedItemPosition == 3) {
                     val adapterr: ArrayAdapter<*> = ArrayAdapter.createFromResource(mainActivity, R.array.group1025, android.R.layout.simple_spinner_item)
                     adapterr.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
                     sp_group1.setAdapter(adapterr)
+                    if (dep1 != -1 && group1 != -1) {
+                        sp_dep1.setSelection(dep1)
+                        sp_group1.setSelection(group1)
+                    }
+                    //SpinnerChanged()
+                    //SpinnerChanged1()
+                    btn_settings.setOnClickListener {
+                        dep1 = sp_dep1.selectedItemPosition
+                        group1 = sp_group1.selectedItemPosition
+                        saveData()
+                    }
+
                 }
                 else if (sp_dep1.selectedItemPosition == 4) {
                     val adapterr: ArrayAdapter<*> = ArrayAdapter.createFromResource(mainActivity, R.array.group4021, android.R.layout.simple_spinner_item)
                     adapterr.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
                     sp_group1.setAdapter(adapterr)
+                    if (dep1 != -1 && group1 != -1) {
+                        sp_dep1.setSelection(dep1)
+                        sp_group1.setSelection(group1)
+                    }
+                    //SpinnerChanged()
+                    //SpinnerChanged1()
+                    btn_settings.setOnClickListener {
+                        dep1 = sp_dep1.selectedItemPosition
+                        group1 = sp_group1.selectedItemPosition
+                        saveData()
+                    }
                 }
                 else if (sp_dep1.selectedItemPosition == 5) {
                     val adapterr: ArrayAdapter<*> = ArrayAdapter.createFromResource(mainActivity, R.array.groupOtd, android.R.layout.simple_spinner_item)
                     adapterr.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
                     sp_group1.setAdapter(adapterr)
+                    if (dep1 != -1 && group1 != -1) {
+                        sp_dep1.setSelection(dep1)
+                        sp_group1.setSelection(group1)
+                    }
+                    //SpinnerChanged()
+                    //SpinnerChanged1()
+                    btn_settings.setOnClickListener {
+                        dep1 = sp_dep1.selectedItemPosition
+                        group1 = sp_group1.selectedItemPosition
+                        saveData()
+                    }
                 }
             }
             override fun onNothingSelected(adapterView: AdapterView<*>?) { }
         })
-        tp_dialog.setIs24HourView(true)
-        //SpinnerChanged()
-        btn_settings.setOnClickListener {
-            saveData()
-        }
-    }
-    
-    fun SpinnerChanged() {
-        val mainActivity = this.activity as MapsActivity
+        /*val mainActivity = this.activity as MapsActivity
         val adapter: ArrayAdapter<*> = ArrayAdapter.createFromResource(mainActivity, R.array.dep, android.R.layout.simple_spinner_item)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         sp_dep1.setAdapter(adapter)
         val adapter1: ArrayAdapter<*> = ArrayAdapter.createFromResource(mainActivity, R.array.group921, android.R.layout.simple_spinner_item)
         adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         sp_group1.setAdapter(adapter1)
-
+        sp_dep1.setSelection(2)
+        sp_group1.setSelection(2)*/
+        //txt_pn.text = sp_dep1.selectedItemPosition.toString()
+        //txt_vt.text = sp_group1.selectedItemPosition.toString()
+    }
+    fun SpinnerChanged1() {
+        group1 = sp_group1.selectedItemPosition
     }
 }
