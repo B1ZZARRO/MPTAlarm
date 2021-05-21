b050c76c46bac858419dba7633e7bc05a47105d6
B1ZZARRO <isip_d.a.jeleznyak@mpt.ru>
2021-05-21 17:37:03 +0300

    21.05

--- app/src/main/java/com/example/mptalarm/fragments/TimerFragment.kt
+++ app/src/main/java/com/example/mptalarm/fragments/TimerFragment.kt
@@ -51,7 +51,7 @@ class TimerFragment : Fragment() {
     private  fun loadData() {
         val mainActivity = this.activity as MapsActivity
         val sharedPreferences = mainActivity.getSharedPreferences("sharedPrefs1", Context.MODE_PRIVATE)
-        val savedInt = sharedPreferences.getInt("string0_KEY",-1)
+        val savedInt = sharedPreferences.getInt("string0_KEY", -1)
         val savedInt1 = sharedPreferences.getInt("string10_KEY", -1)
         dep1 = savedInt
         group1 = savedInt1
@@ -61,14 +61,14 @@ class TimerFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         loadData()
         tp_dialog.setIs24HourView(true)
-        val mainActivity = this.activity as MapsActivity
-        val adapter: ArrayAdapter<*> = ArrayAdapter.createFromResource(mainActivity, R.array.dep, android.R.layout.simple_spinner_item)
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        sp_dep1.setAdapter(adapter)
-        /*val adapter1: ArrayAdapter<*> = ArrayAdapter.createFromResource(mainActivity, R.array.group921, android.R.layout.simple_spinner_item)
-        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        sp_group1.setAdapter(adapter1)*/
+        SpinnerChanged1()
+        btn_settings.setOnClickListener {
+            dep1 = sp_dep1.selectedItemPosition
+            group1 = sp_group1.selectedItemPosition
+            saveData()
+        }
         SpinnerChanged()
+
     }
     
     fun SpinnerChanged() {
@@ -79,98 +79,31 @@ class TimerFragment : Fragment() {
                     val adapterr: ArrayAdapter<*> = ArrayAdapter.createFromResource(mainActivity, R.array.group921, android.R.layout.simple_spinner_item)
                     adapterr.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
                     sp_group1.setAdapter(adapterr)
-                    if (dep1 != -1 && group1 != -1) {
-                        sp_dep1.setSelection(dep1)
-                        sp_group1.setSelection(group1)
-                    }
-                    //SpinnerChanged()
-                    //SpinnerChanged1()
-                    btn_settings.setOnClickListener {
-                        dep1 = sp_dep1.selectedItemPosition
-                        group1 = sp_group1.selectedItemPosition
-                        saveData()
-                    }
                 }
                 else if (sp_dep1.selectedItemPosition == 1) {
                     val adapterr: ArrayAdapter<*> = ArrayAdapter.createFromResource(mainActivity, R.array.group926, android.R.layout.simple_spinner_item)
                     adapterr.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
                     sp_group1.setAdapter(adapterr)
-                    if (dep1 != -1 && group1 != -1) {
-                        sp_dep1.setSelection(dep1)
-                        sp_group1.setSelection(group1)
-                    }
-                    //SpinnerChanged()
-                    //SpinnerChanged1()
-                    btn_settings.setOnClickListener {
-                        dep1 = sp_dep1.selectedItemPosition
-                        group1 = sp_group1.selectedItemPosition
-                        saveData()
-                    }
                 }
                 else if (sp_dep1.selectedItemPosition == 2) {
                     val adapterr: ArrayAdapter<*> = ArrayAdapter.createFromResource(mainActivity, R.array.group927, android.R.layout.simple_spinner_item)
                     adapterr.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
                     sp_group1.setAdapter(adapterr)
-                    if (dep1 != -1 && group1 != -1) {
-                        sp_dep1.setSelection(dep1)
-                        sp_group1.setSelection(group1)
-                    }
-                    //SpinnerChanged()
-                    //SpinnerChanged1()
-                    btn_settings.setOnClickListener {
-                        dep1 = sp_dep1.selectedItemPosition
-                        group1 = sp_group1.selectedItemPosition
-                        saveData()
-                    }
                 }
                 else if (sp_dep1.selectedItemPosition == 3) {
                     val adapterr: ArrayAdapter<*> = ArrayAdapter.createFromResource(mainActivity, R.array.group1025, android.R.layout.simple_spinner_item)
                     adapterr.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
                     sp_group1.setAdapter(adapterr)
-                    if (dep1 != -1 && group1 != -1) {
-                        sp_dep1.setSelection(dep1)
-                        sp_group1.setSelection(group1)
-                    }
-                    //SpinnerChanged()
-                    //SpinnerChanged1()
-                    btn_settings.setOnClickListener {
-                        dep1 = sp_dep1.selectedItemPosition
-                        group1 = sp_group1.selectedItemPosition
-                        saveData()
-                    }
-
                 }
                 else if (sp_dep1.selectedItemPosition == 4) {
                     val adapterr: ArrayAdapter<*> = ArrayAdapter.createFromResource(mainActivity, R.array.group4021, android.R.layout.simple_spinner_item)
                     adapterr.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
                     sp_group1.setAdapter(adapterr)
-                    if (dep1 != -1 && group1 != -1) {
-                        sp_dep1.setSelection(dep1)
-                        sp_group1.setSelection(group1)
-                    }
-                    //SpinnerChanged()
-                    //SpinnerChanged1()
-                    btn_settings.setOnClickListener {
-                        dep1 = sp_dep1.selectedItemPosition
-                        group1 = sp_group1.selectedItemPosition
-                        saveData()
-                    }
                 }
                 else if (sp_dep1.selectedItemPosition == 5) {
                     val adapterr: ArrayAdapter<*> = ArrayAdapter.createFromResource(mainActivity, R.array.groupOtd, android.R.layout.simple_spinner_item)
                     adapterr.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
                     sp_group1.setAdapter(adapterr)
-                    if (dep1 != -1 && group1 != -1) {
-                        sp_dep1.setSelection(dep1)
-                        sp_group1.setSelection(group1)
-                    }
-                    //SpinnerChanged()
-                    //SpinnerChanged1()
-                    btn_settings.setOnClickListener {
-                        dep1 = sp_dep1.selectedItemPosition
-                        group1 = sp_group1.selectedItemPosition
-                        saveData()
-                    }
                 }
             }
             override fun onNothingSelected(adapterView: AdapterView<*>?) { }
@@ -188,6 +121,51 @@ class TimerFragment : Fragment() {
         //txt_vt.text = sp_group1.selectedItemPosition.toString()
     }
     fun SpinnerChanged1() {
-        group1 = sp_group1.selectedItemPosition
+        val mainActivity = this.activity as MapsActivity
+        if (dep1 != -1 && group1 != -1) {
+            val adapter: ArrayAdapter<*> = ArrayAdapter.createFromResource(mainActivity, R.array.dep, android.R.layout.simple_spinner_item)
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            sp_dep1.setAdapter(adapter)
+            if (dep1 == 0) {
+                val adapter: ArrayAdapter<*> = ArrayAdapter.createFromResource(mainActivity, R.array.group921, android.R.layout.simple_spinner_item)
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                sp_group1.setAdapter(adapter)
+            }
+            else if (dep1 == 1) {
+                val adapter: ArrayAdapter<*> = ArrayAdapter.createFromResource(mainActivity, R.array.group926, android.R.layout.simple_spinner_item)
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                sp_group1.setAdapter(adapter)
+            }
+            else if (dep1 == 2) {
+                val adapter: ArrayAdapter<*> = ArrayAdapter.createFromResource(mainActivity, R.array.group927, android.R.layout.simple_spinner_item)
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                sp_group1.setAdapter(adapter)
+            }
+            else if (dep1 == 3) {
+                val adapter: ArrayAdapter<*> = ArrayAdapter.createFromResource(mainActivity, R.array.group1025, android.R.layout.simple_spinner_item)
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                sp_group1.setAdapter(adapter)
+            }
+            else if (dep1 == 4) {
+                val adapter: ArrayAdapter<*> = ArrayAdapter.createFromResource(mainActivity, R.array.group4021, android.R.layout.simple_spinner_item)
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                sp_group1.setAdapter(adapter)
+            }
+            else if (dep1 == 5) {
+                val adapter: ArrayAdapter<*> = ArrayAdapter.createFromResource(mainActivity, R.array.groupOtd, android.R.layout.simple_spinner_item)
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                sp_group1.setAdapter(adapter)
+            }
+            sp_dep1.setSelection(dep1)
+            sp_group1.setSelection(group1)
+        }
+        else {
+            val adapter: ArrayAdapter<*> = ArrayAdapter.createFromResource(mainActivity, R.array.dep, android.R.layout.simple_spinner_item)
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            sp_dep1.setAdapter(adapter)
+            val adapter1: ArrayAdapter<*> = ArrayAdapter.createFromResource(mainActivity, R.array.group921, android.R.layout.simple_spinner_item)
+            adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            sp_group1.setAdapter(adapter1)
+        }
     }
 }
